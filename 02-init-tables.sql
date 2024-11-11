5d01fee0b4fd5b4961f2d6a58f8a2094d2cd0fe2
PinchToDebug <52333417+PinchToDebug@users.noreply.github.com>
2024-11-11 13:41:15 +0100

    pets.weight fix

--- 02-init-tables.sql
+++ 02-init-tables.sql
@@ -6,7 +6,7 @@ CREATE TABLE pets(
     color VARCHAR2(30 CHAR),
     breed VARCHAR2(50 CHAR),
     age NUMBER,
-    weight NUMBER,             -- Weight in kilogram
+    weight NUMBER(5, 2),       -- Weight in kilogram
     status VARCHAR2(10 CHAR),  -- Available / Adopted / Fostered
     notes VARCHAR2(300 CHAR),  -- Description of the pet
     added_at TIMESTAMP
