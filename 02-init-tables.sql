12e6b4d126214214232ca409e0a34027cbaaa036
PinchToDebug <52333417+PinchToDebug@users.noreply.github.com>
2024-11-11 16:13:31 +0100

    Formatting

--- 02-init-tables.sql
+++ 02-init-tables.sql
@@ -2,7 +2,7 @@
 CREATE TABLE pets(
     pet_id   NUMBER PRIMARY KEY,
     pet_name VARCHAR2(100 CHAR) NOT NULL,
-    species  VARCHAR2(50 CHAR) NOT NULL,
+    species  VARCHAR2(50 CHAR)  NOT NULL,
     color    VARCHAR2(30 CHAR),
     breed    VARCHAR2(50 CHAR),
     age      NUMBER,
