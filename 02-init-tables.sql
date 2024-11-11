997fc7d2bae60c5f5ab784216935257851f773f5
PinchToDebug <52333417+PinchToDebug@users.noreply.github.com>
2024-11-11 13:29:34 +0100

    Typo fix

--- 02-init-tables.sql
+++ 02-init-tables.sql
@@ -2,12 +2,12 @@
 CREATE TABLE pets(
     pet_id NUMBER PRIMARY KEY,
     pet_name VARCHAR2(100 CHAR) NOT NULL,
-    species CHAR(50 CHAR) NOT NULL,
+    species VARCHAR2(50 CHAR) NOT NULL,
     color VARCHAR2(30 CHAR),
     breed VARCHAR2(50 CHAR),
     age NUMBER,
     weight NUMBER,             -- Weight in kilogram
-    status VARCHAR2(10 CHAR),  -- Available / Adopted
+    status VARCHAR2(10 CHAR),  -- Available / Adopted / Fostered
     notes VARCHAR2(300 CHAR),  -- Description of the pet
     added_at TIMESTAMP
 )
