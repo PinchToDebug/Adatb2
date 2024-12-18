6a1fee3e6b420e10b04e455479e16abd1614086a
PinchToDebug <52333417+PinchToDebug@users.noreply.github.com>
2024-12-18 16:53:41 +0100

    Format fix

--- 03_sq_sequences.sql
+++ 03_sq_sequences.sql
@@ -1,8 +1,8 @@
-CREATE SEQUENCE seq_pet_id START WITH 1000;          -- seq for  pet               table
-CREATE SEQUENCE seq_request_id START WITH 1;         -- seq for  adoption_request  table
-CREATE SEQUENCE seq_adopter_id START WITH 1000;      -- seq for  adopter           table
-CREATE SEQUENCE seq_record_id START WITH 10000;      -- seq for  MEDICAL_HISTORY    table
-CREATE SEQUENCE seq_veterinarian_id START WITH 100;  -- seq for  veterinarian      table
-CREATE SEQUENCE seq_surgery_id START WITH 1;         -- seq for  surgery          table
-CREATE SEQUENCE seq_vaccine_id START WITH 1;         -- seq for  vaccine           table
-CREATE SEQUENCE seq_log_id START WITH 1;             -- seq for  LOG                table
+CREATE SEQUENCE seq_pet_id START WITH 1000;          -- seq for  PET               table
+CREATE SEQUENCE seq_request_id START WITH 1;         -- seq for  ADOPTION_REQUEST  table
+CREATE SEQUENCE seq_adopter_id START WITH 1000;      -- seq for  ADOPTER           table
+CREATE SEQUENCE seq_record_id START WITH 10000;      -- seq for  MEDICAL_HISTORY   table
+CREATE SEQUENCE seq_veterinarian_id START WITH 100;  -- seq for  VETERINARIAN      table
+CREATE SEQUENCE seq_surgery_id START WITH 1;         -- seq for  SURGERY           table
+CREATE SEQUENCE seq_vaccine_id START WITH 1;         -- seq for  VACCINE           table
+CREATE SEQUENCE seq_log_id START WITH 1;             -- seq for  LOG               table
